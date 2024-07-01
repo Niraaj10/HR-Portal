979445d59ac1e005e8580e9e25dca3d04c4bb958
Niraaj10 <nitcode10@gmail.com>
2024-07-02 00:01:40 +0530

    .

--- src/components/LeaveReq.jsx
+++ src/components/LeaveReq.jsx
@@ -26,13 +26,15 @@ const LeaveReq = ({ user, userID, userRole }) => {
       <div className='LeavePage bg-white p-7 mt-5 mr-10 rounded-xl h-[70vh] flex gap-5'>
 
 
-        <div className="ReqCont w-[70%]">
-          Leave Request Page
+        <div className="ReqCont flex flex-col items-center w-[70%]">
+          <div className='text-xl mb-5'>
+            Leave Request
+          </div>
 
           <form action="">
 
 
-            <div>
+            <div className='flex gap-14 px-9 justify-center'>
               <div>
                 <label htmlFor="startDate">Start Date: </label>
                 <input type="date" id="startDate" value={startDate} onChange={(e) => setStartDate(e.target.value)} required />
@@ -42,24 +44,54 @@ const LeaveReq = ({ user, userID, userRole }) => {
                 <label htmlFor="endDate">End Date :</label>
                 <input type="date" id="endDate" value={endDate} onChange={(e) => setEndDate(e.target.value)} required />
               </div>
-
             </div>
 
-            
 
-            <div>
-              <label htmlFor="LeaveType">Leave Type: </label>
-              <div>
-                <input type="radio" id='vacation' name='leaveType' value="Vacation" checked={lType === 'Vacation'} onChange={() => setLType('Vacation')} />
-                <label htmlFor="vacation">Vacation</label>
-              </div>
-              <div>
-                <input type="radio" id='medical' name='leaveType' value="Medical" checked={lType === 'Medical'} onChange={() => setLType('Medical')} />
-                <label htmlFor="medical">Medical</label>
-              </div>
-              <div>
-                <input type="radio" id='other' name='leaveType' value="Other" checked={lType === 'Other'} onChange={() => setLType('Other')} />
-                <label htmlFor="other">Other</label>
+
+            <div className=''>
+              <label htmlFor="LeaveType" className=''>Leave Type: </label>
+
+              <div className='flex gap-4 w-[100%] justify-center px-10'>
+
+                <div className='flex flex-col justify-center items-end '>
+                  <input type="radio" id='vacation' name='leaveType' value="vacation" checked={lType === 'Vacation'} onChange={() => setLType('Vacation')} className='peer mb-3 hidden' />
+                  <label htmlFor="vacation" className=' justify-between text-[#3354F4] w-full border border-2 border-[#3354F4] peer-checked:bg-[#3354F4] peer-checked:text-white p-5 px-9 rounded-2xl hover:bg-[#2d50ff] hover:text-white hover:border-white'>
+                    Vacation
+                  </label>
+                </div>
+
+                <div className=' flex flex-col justify-center items-end'>
+                  <input type="radio" id='medical' name='leaveType' value="Medical" checked={lType === 'Medical'} onChange={() => setLType('Medical')} className='peer mb-3 hidden' />
+                  <label htmlFor="medical" className=' justify-between text-[#3354F4] w-full border border-2 border-[#3354F4] peer-checked:bg-[#3354F4] peer-checked:text-white p-5 px-9 rounded-2xl hover:bg-[#2d50ff] hover:text-white hover:border-white'>
+                    Medical
+                  </label>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center'>
+                  <input type="radio" id='casual' name='leaveType' value="casual" checked={lType === 'Casual'} onChange={() => setLType('Casual')} className='peer mb-3 hidden' />
+                  <label htmlFor="casual" className=' justify-between text-[#3354F4] w-fullborder border-2 border-[#3354F4] peer-checked:bg-[#3354F4] peer-checked:text-white p-5 px-9 rounded-2xl hover:bg-[#2d50ff] hover:text-white hover:border-white'>
+                    Casual
+                  </label>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center'>
+                  <input type="radio" id='mat' name='leaveType' value="mat" checked={lType === 'Maternity'} onChange={() => setLType('Maternity')} className='peer mb-3 hidden' />
+                  <label htmlFor="mat" className=' justify-between text-[#3354F4] w-fullborder border-2 border-[#3354F4] peer-checked:bg-[#3354F4] peer-checked:text-white p-5 px-9 rounded-2xl hover:bg-[#2d50ff] hover:text-white hover:border-white'>
+                    Maternity
+                  </label>
+                </div>
+
+                <div className=' flex flex-col justify-center items-center'>
+                  <input type="radio" id='other' name='leaveType' value="Other" checked={lType === 'Other'} onChange={() => setLType('Other')} className='peer mb-3 hidden' />
+                  <label htmlFor="other" className=' justify-between text-[#3354F4] w-fullborder border-2 border-[#3354F4] peer-checked:bg-[#3354F4] peer-checked:text-white p-5 px-9 rounded-2xl hover:bg-[#2d50ff] hover:text-white hover:border-white'>
+                    Other
+                  </label>
+                </div>
+
+
+
+
+
               </div>
 
             </div>
